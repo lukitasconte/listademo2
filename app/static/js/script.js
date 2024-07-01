55e7fe0fa4980f262ab38f59b15449c9272102fd
lukitasconte <138738151+lukitasconte@users.noreply.github.com>
2024-07-01 00:30:59 -0300

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -7,70 +7,53 @@ document.addEventListener('DOMContentLoaded', function() {
 
     button.addEventListener('click', function() {
         if (form.style.display === 'none') {
-            // Mostrar el formulario y ocultar el historial de tareas
             form.style.display = 'block';
             history.style.display = 'none';
-            button.innerHTML = '⌃'; // Cambiar a flecha hacia arriba
+            button.innerHTML = '⌃';
         } else {
-            // Ocultar el formulario y mostrar el historial de tareas
             form.style.display = 'none';
             history.style.display = 'block';
-            button.innerHTML = '+'; // Cambiar a símbolo de más
+            button.innerHTML = '+';
         }
     });
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-
-        // Obtener los valores del formulario
         const title = document.getElementById('taskTitle').value;
         const description = document.getElementById('taskDescription').value;
 
         if (form.dataset.mode === 'edit') {
-            // Modo edición: actualizar la tarea existente
             const taskCard = document.querySelector('.task-card.editing');
             const titleElement = taskCard.querySelector('h4');
             const descriptionElement = taskCard.querySelector('p');
 
-            // Actualizar los elementos de tarea con los nuevos valores
             titleElement.textContent = title;
             descriptionElement.textContent = description;
 
-            // Cambiar el modo del formulario de vuelta a 'add'
             form.dataset.mode = 'add';
             form.querySelector('button[type="submit"]').textContent = 'Guardar Tarea';
 
-            // Mostrar la tarea actualizada y limpiar el formulario
             taskCard.classList.remove('editing');
             form.reset();
         } else {
-            // Modo agregar nueva tarea
-            // Elegir un color aleatorio de la lista de colores
             const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-            // Crear un nuevo elemento de tarea con el color elegido
             const taskCard = createTaskCard(title, description, randomColor);
             
             taskContainer.appendChild(taskCard);
-
-            // Limpiar el formulario
             form.reset();
         }
 
-        // Ocultar el formulario y mostrar el historial de tareas
         form.style.display = 'none';
         history.style.display = 'block';
-        button.innerHTML = '+'; // Cambiar a símbolo de más
+        button.innerHTML = '+';
     });
 
-    // Añadir funcionalidad para el botón Enter
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter' && form.style.display === 'block') {
             form.dispatchEvent(new Event('submit'));
         }
     });
 
-    // Función para crear el elemento de tarea
     function createTaskCard(title, description, color) {
         const taskCard = document.createElement('div');
         taskCard.className = `task-card ${color}`;
@@ -90,14 +73,12 @@ document.addEventListener('DOMContentLoaded', function() {
             </div>
         `;
 
-        // Botón para cambiar el color de fondo
         const colorSelector = createColorSelector(taskCard);
         taskCard.appendChild(colorSelector);
 
         return taskCard;
     }
 
-    // Función para crear el selector de color
     function createColorSelector(taskCard) {
         const colorOptions = document.createElement('div');
         colorOptions.className = 'color-options';
@@ -106,8 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const colorOption = document.createElement('div');
             colorOption.className = `color-option ${color}`;
             colorOption.addEventListener('click', function(event) {
-                event.stopPropagation(); // Evitar que el click se propague al formulario
-
+                event.stopPropagation();
                 taskCard.className = `task-card ${color}`;
             });
             colorOptions.appendChild(colorOption);
@@ -116,36 +96,28 @@ document.addEventListener('DOMContentLoaded', function() {
         return colorOptions;
     }
 
-    // Función para editar la tarea
     window.editTask = function(editButton) {
         const taskCard = editButton.parentNode.parentNode;
         const titleElement = taskCard.querySelector('h4');
         const descriptionElement = taskCard.querySelector('p');
         
-        // Mostrar el formulario de edición con los valores actuales
         form.style.display = 'block';
         history.style.display = 'none';
-        button.innerHTML = '⌃'; // Cambiar a flecha hacia arriba
+        button.innerHTML = '⌃';
 
-        // Prellenar el formulario con los valores actuales de la tarea
         document.getElementById('taskTitle').value = titleElement.textContent;
         document.getElementById('taskDescription').value = descriptionElement.textContent;
 
-        // Marcar la tarea como editándose
         taskCard.classList.add('editing');
-
-        // Cambiar el modo del formulario a 'edit'
         form.dataset.mode = 'edit';
         form.querySelector('button[type="submit"]').textContent = 'Guardar Cambios';
     };
 
-    // Función para marcar una tarea como hecha
     window.markTaskDone = function(doneButton) {
         const taskCard = doneButton.parentNode.parentNode;
         taskCard.classList.toggle('done');
     };
 
-    // Función para confirmar la eliminación de una tarea
     window.confirmDeleteTask = function(deleteButton) {
         if (confirm('¿Desea eliminar esta tarea?')) {
             const taskCard = deleteButton.parentNode.parentNode;
@@ -153,7 +125,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 
-    // Función para cambiar el cursor al pasar sobre el icono de editar
     window.changeCursor = function(element) {
         element.style.cursor = 'pointer';
     };
@@ -165,7 +136,6 @@ function updateClock() {
     var minutes = now.getMinutes();
     var seconds = now.getSeconds();
 
-    // Formatear los números para que tengan dos dígitos
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
@@ -173,14 +143,48 @@ function updateClock() {
     var timeString = hours + ':' + minutes + ':' + seconds;
     document.getElementById('time').textContent = timeString;
 
-    // Opcional: mostrar la fecha
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var dateString = now.toLocaleDateString(undefined, options);
     document.getElementById('date').textContent = dateString;
 }
 
-// Actualizar el reloj cada segundo
 setInterval(updateClock, 1000);
-
-// Llamar a la función inmediatamente para mostrar el reloj al cargar la página
 updateClock();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const themeSwitch = document.getElementById('toggleThemeSwitch');
+    const languageSelect = document.getElementById('languageSelect');
+
+    themeSwitch.addEventListener('change', () => {
+        document.body.classList.toggle('dark-theme', themeSwitch.checked);
+        localStorage.setItem('theme', themeSwitch.checked ? 'dark' : 'light');
+    });
+
+    languageSelect.addEventListener('change', () => {
+        const selectedLanguage = languageSelect.value;
+        changeLanguage(selectedLanguage);
+        localStorage.setItem('selectedLanguage', selectedLanguage);
+    });
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+        themeSwitch.checked = true;
+    }
+
+    const savedLanguage = localStorage.getItem('selectedLanguage');
+    if (savedLanguage) {
+        languageSelect.value = savedLanguage;
+        changeLanguage(savedLanguage);
+    }
+
+    function changeLanguage(language) {
+        const elements = document.querySelectorAll('[data-text-es], [data-text-en]');
+        elements.forEach(element => {
+            const textKey = element.getAttribute(`data-text-${language}`);
+            if (textKey) {
+                element.textContent = textKey;
+            }
+        });
+    }
+});
